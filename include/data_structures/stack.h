2680c4d7ba03027d61a6b1515d3e919aed5a9e52
shreejan <shr.shreejan@gmail.com>
2025-08-17 23:29:36 +0930

    Added placeholder files for stack data structure

---
+++ include/data_structures/stack.h
@@ -0,0 +1,8 @@
+#ifndef STACK_H_
+#define STACK_H_
+
+#include <stdio.h>
+
+void Stack(void);
+
+#endif
---
+++ src/data_structures/stack.c
@@ -0,0 +1,6 @@
+#include "data_structures/stack.h"
+
+void Stack(void)
+{
+    printf("Stack\n");
+}
